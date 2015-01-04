4884746d6117895a8d8f321483b5bf5f22ea4838
Victor Tavares <vtavares00dev@gmail.com>
2015-01-04 00:31:23 -0500

    changes to warnings flags

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -38,11 +38,7 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
+'-pedantic',
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
 # source code needs it.
 '-DUSE_CLANG_COMPLETER',
